0ff49f5e262d677e4313e61584e49207e2f0ff31
Shelbynord2017 <shelbynord2017@gmail.com>
2026-06-05 22:11:34 -0400

    navigate update

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
 import './Navbar.css'
 import recipes_icon from '../../assets/recipes_icon.png'
+import { useNavigate } from 'react-router-dom'
 
 const Navbar = () => {
+
+const navigate = useNavigate()
+
   return (
     <div>
       <div className="nav__container">
@@ -11,8 +15,8 @@ const Navbar = () => {
               <img src={recipes_icon} alt="Logo" className="nav__logo--img"/>
             </figure>
             <div className="nav__links">
-              <a href="/" className="nav__link">Home</a>
-              <a href="/recipes" className="nav__link">Recipes</a>
+              <a onClick={() => navigate('/')} className="nav__link">Home</a>
+              <a onClick={() => navigate('/recipes')} href="/recipes" className="nav__link">Recipes</a>
               <a href="#" className="nav__link">Contact</a>
             </div>
         </div>
